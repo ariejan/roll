b72b076af8b18ef4f6b10296f12840f23258acec
Ariejan de Vroom <ariejan@ariejan.net>
2015-10-12 12:45:49 +0200

    Update version rendering

--- commands/version.go
+++ commands/version.go
@@ -12,6 +12,6 @@ var version = &cobra.Command{
 	Short: "Print the version number of Roll",
 	Long:  `This is my version, there are many like it, but this one is mine.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Roll %s\n", core.Version)
+		fmt.Println(core.Version)
 	},
 }
--- core/version.go
+++ core/version.go
@@ -5,16 +5,17 @@ import "fmt"
 type version struct {
 	Major, Minor, Patch int
 	Label               string
+	Name                string
 }
 
-var Version = version{1, 2, 3, "dev"}
+var Version = version{1, 2, 3, "dev", "Chuck Norris"}
 
 var Build string
 
 func (v version) String() string {
 	if v.Label != "" {
-		return fmt.Sprintf("Roll version %d.%d.%d-%s\nGit commit hash: %s", v.Major, v.Minor, v.Patch, v.Label, Build)
+		return fmt.Sprintf("Roll version %d.%d.%d-%s \"%s\"\nGit commit hash: %s", v.Major, v.Minor, v.Patch, v.Label, v.Name, Build)
 	} else {
-		return fmt.Sprintf("Roll version %d.%d.%d\nGit commit hash: %s", v.Major, v.Minor, v.Patch, Build)
+		return fmt.Sprintf("Roll version %d.%d.%d \"%s\"\nGit commit hash: %s", v.Major, v.Minor, v.Patch, v.Name, Build)
 	}
 }
